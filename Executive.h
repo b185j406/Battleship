49bccca3a7b2b76e09c8c774c4ecee726e8d59c2
Jake Beesley <jakebees@gmail.com>
2021-02-17 14:44:29 -0600

    Added the Executive.h file to the repository. Has a run function that is initially called by main.

---
+++ Executive.h
@@ -0,0 +1,20 @@
+//Executive.h
+
+#ifndef EXECUTIVE_H
+#define EXECUTIVE_H
+#include <string>
+#include <fstream>
+#include <iostream>
+
+class Executive
+{
+
+	public:
+		//Executive();
+		void run();
+		//~Executive();
+
+	private:
+
+};
+#endif
