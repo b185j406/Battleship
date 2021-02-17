2658701f88f658434eeeaa8b7700f4769715c12c
Jake Beesley <jakebees@gmail.com>
2021-02-17 14:45:04 -0600

    Added the executive.cpp file to the repository. Currently only has a void function called run, which outputs a statement to the command line.

---
+++ Executive.cpp
@@ -0,0 +1,15 @@
+//Executive.cpp
+
+#include "Executive.h"
+#include <iostream>
+
+
+//Executive::Executive() //constructor
+//{
+
+//}
+
+void Executive::run() //runs the program
+{
+	std::cout << "Got here! \n";
+}
