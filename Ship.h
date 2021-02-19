4a2ae38ba93c537e43d7e24f1ad8337ef10383bd
Jake Beesley <jakebees@gmail.com>
2021-02-19 12:28:05 -0600

    Created a header file for the ship class. The ship class is responsible for knowing the length, direction, and start of the ship.
    From there, an array needs to be implemented that is the length of the ship,
    where each element of the array holds the coordinates of a ship.
    (i.e. Ship of length four, vertical orientation, starts at A1, would
    hold the coordinates A1, A2, A3, and A4.)

---
+++ Ship.h
@@ -0,0 +1,29 @@
+//Ship.h
+
+#ifndef SHIP_H
+#define SHIP_H
+#include <string>
+#include <fstream>
+#include <iostream>
+
+class Ship
+{
+
+	public:
+
+
+		Ship(std::string shipStart, std::string shipOrientation, int length);
+
+		bool isDestroyed(); //returns true if shipIntegrity is zero
+
+		bool isHit(int row, char col);
+
+	private:
+		int m_shipLength = 0;
+		int m_hullIntegrity = 0;
+		std::string m_shipHead;
+		std::string m_shipDirection;
+
+
+};
+#endif
