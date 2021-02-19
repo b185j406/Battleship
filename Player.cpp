fe7511a613a74aee445640fb75f95a3d91949fa0
Jake Beesley <jakebees@gmail.com>
2021-02-19 16:06:19 -0600

    Added functions to the Player class:   - validateCol : Takes a string and verifies its within A-J   - validateRow : Takes an int and verifies its within 1-10   - intToString : Takes an int and converts it to a string
    Added functions allow for storing the coordinates and orientation of a ship.
    If passed to ship constructor, "should" create ship with valid info due to
    validation functions and methods implemented in the player class.

--- Player.cpp
+++ Player.cpp
@@ -37,56 +37,142 @@ void Player::showWaters(){
 	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
 }
 
-void Player::anchorShips(int ships){
-	char shipStarterCol = 0;
+bool Player::validateCol(std::string column){
+	bool isValid = false;
+	do{
+		if(column == "A" || column == "a"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "B" || column == "b"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "C" || column == "b"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "D" || column == "d"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "E" || column == "e"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "F" || column == "f"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "G" || column == "g"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "H" || column == "h"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "I" || column == "i"){
+			isValid = true;
+			return(true);
+		}
+		else if(column == "J" || column == "j"){
+			isValid = true;
+			return(true);
+		}
+		else{
+			isValid = false;
+			return(false);
+		}
+	}while(isValid == false);
+}
+
+std::string Player::intToString(int num){
+	std::string temp = std::to_string(num);
+	return(temp);
+}
+
+bool Player::validateRow(int row){
+	bool isValid = false;
+	do{
+		if(row == 1){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 2){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 3){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 4){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 5){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 6){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 7){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 8){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 9){
+			isValid = true;
+			return(true);
+		}
+		else if(row == 10){
+			isValid = true;
+			return(true);
+		}
+	}while(isValid == false);
+}
+
+void Player::anchorShips(){
+	std::string shipStarterCol;
 	int shipStarterRow = 0;
 	std::string shipLocation;
 	std::string shipPlacement;
 
 	std::cout << "\nWhat orientation would you like the ship to be placed in?\n";
-	std::cout << "Vertical or Horizontal? (V / H):\n";
-	std::cin >> shipPlacement;
-	while(shipPlacement != "V" && shipPlacement != "v" && shipPlacement != "H" && shipPlacement != "h")// fails on anything thats not a V or a v or a H or a h
-		std::cin.clear();
-		std::cin.ignore(100, '\n');
-		std::cout << "Vertical or Horizontal? (V / H):\n";
+
+	do{
+		std::cout << "Vertical or Horizontal? (V / H): ";
 		std::cin >> shipPlacement;
-	}
+	}while(!std::cin.fail() && shipPlacement != "V" && shipPlacement != "v" && shipPlacement != "H" && shipPlacement != "h");
 
-	//NOTE TO JAKE IN THE FUTURE
-
-	//NEED TO VALIDATE THE v V or H h FOR PLACING THE SHIP. NEED TO GET VALID INFO BEFORE WE CAN PASS TO SHIP/PLAYER STUFF!!!
-
-
-
-
-
-
-
-
-	/*
 	std::cout << "\nWhere would you like to place the head of the ship? \n";
-	std::cout << "Please input a valid column (A - J): ";
-	std::cin >> shipStarterCol;
-	while(shipStarterCol != 'A' || shipStarterCol != 'B' || shipStarterCol != 'C' || shipStarterCol != 'D' || shipStarterCol != 'E' || shipStarterCol != 'F' || shipStarterCol != 'G' || shipStarterCol != 'H' || shipStarterCol != 'I' || shipStarterCol != 'J' || std::cin.fail()){
-		std::cin.clear();
-		std::cin.ignore(100, '\n');
-		std::cout << "Please input a valid column: ";
+	do{
+		std::cout << "Please input a valid column (A - J): ";
 		std::cin >> shipStarterCol;
-	}
-	std::cout << "Please input a valid row (1 - 10): ";
-	std::cin >> shipStarterRow;
-	while(shipStarterRow < 1 || shipStarterRow > 10 || std::cin.fail()){
-		std::cin.clear();
-		std::cin.ignore(100, '\n');
-		std::cout << "Please input a valid column: ";
-		std::cin >> shipStarterRow;;
-	}
-	shipLocation = shipStarterCol + shipStarterRow;
+	}while(validateCol(shipStarterCol) == false);
 
-	std::cout << shipLocation << "\n";
-	*/
+	do{
+		std::cout << "Please input a valid row (1 - 10): ";
+		std::cin >> shipStarterRow;
+	}while(validateRow(shipStarterRow) == false);
 
+		std::cout << "\n\nV or H? : " << shipPlacement << "\n"; //Tester statement
+		std::cout << "Column : " << shipStarterCol << "\n";
+		std::cout << "Row : " << shipStarterRow << "\n";
+
+		shipLocation = shipStarterCol + intToString(shipStarterRow);
+
+		std::cout << "Head location is: " << shipLocation <<"\n";
+
+	//std::cout << shipLocation << "\n";
+
+}
 
 	//for(int i = 1; i <= ships; i++){
 		//Ship(stringstart, shipPlacement, i); //i is the length of the ship
