eebbe08b5bfb4c397bc366efe0acbc750bc9df48
Chloe Thornton <chloe.kira@gmail.com>
2021-02-20 12:23:17 -0600

    Worked on create array and is Hit, both should work now. Still need shipdocks() and something in executive or player that reads isDestroyed() output and does isWinner()

--- Ship.cpp
+++ Ship.cpp
@@ -9,25 +9,104 @@ Ship::Ship(std::string shipStart, std::string shipOrientation, int length){ //co
 	m_hullIntegrity = length;
 	m_shipHead = shipStart;
 	m_shipDirection = shipOrientation;
+	for(int i=0;i<6;i++){//makes sure the array never has an empty element
+		m_shipArray[i]=0;
+	}
+}
+
+int Ship::colToNum(std::string column){
+	if(column == "A" || column == "a"){
+		return(1);
+	}
+	else if(column == "B" || column == "b"){
+		return(2);
+	}
+	else if(column == "C" || column == "c"){
+		return(3);
+	}
+	else if(column == "D" || column == "d"){
+		return(4);
+	}
+	else if(column == "E" || column == "e"){
+		return(5);
+	}
+	else if(column == "F" || column == "f"){
+		return(6);
+	}
+	else if(column == "G" || column == "g"){
+		return(7);
+	}
+	else if(column == "H" || column == "h"){
+		return(8);
+	}
+	else if(column == "I" || column == "i"){
+		return(9);
+	}
+	else if(column == "J" || column == "j"){
+		return(10);
+	}
+	return(0);
+}
+
+std::string Ship::numToString(int num){
+	std::string temp = std::to_string(num);
+	return(temp);
+}
+
+std::string Ship::numToCol(int colnum){
+	if(colnum = 1){
+		return("A");
+	}
+	else if(colnum = 2){
+		return("B");
+	}
+	else if(colnum = 3){
+		return("C");
+	}
+	else if(colnum = 4){
+		return("D");
+	}
+	else if(colnum = 5){
+		return("E");
+	}
+	else if(colnum = 6){
+		return("F");
+	}
+	else if(colnum = 7){
+		return("G");
+	}
+	else if(colnum = 8){
+		return("H");
+	}
+	else if(colnum = 9){
+		return("I");
+	}
+	else if(colnum = 10){
+		return("J");
+	}
+	return("X");//shouldn't ever happen because validate posiiton checks for this
 }
 
 void Ship::createArray(){
 
-	//Need to read the m_shipHead to know where to start
-	//Need to read m_shipDirection to know whether to go LEFT or DOWN
+	char headcol = m_shiphead.at(0);
+	char headrow= m_shiphead.at(1);
+	int hrow = headrow - '0';
+	int hcol = colToNum(headcol);
+	std::string colstring(1,headcol);
 
-	//Coordinates are validated in the player class. Therefore this function
-	//And the valid coordinate function are pretty similar. If one is implemented
-	//The other should be able to borrow most of the code.
-
-	//The issue with createArray is that you are handed the starting position, the direction, and the length.
-	//From there, you need to infer what coordinates to add next.
-
-	//For example, I am using createArray() on a ship that has its head at A1, its direction Vertical, and its length is 5.
-	//This function would have been validated by the player function, so you know the directions you are going are valid.
-	//Therefore, the array would be [A1, A2, A3, A4, A5, 0] since the ship array is already 6 elements long.
-
-
+	if(m_shipDirection=="V"){
+	for(int i=0;i<size;i++){
+			hrow = hrow+i;
+			m_shipArray[i]= colstring+numToString(hrow);
+		}
+	}
+	if(m_shipDirection=="H"){
+	for(int i=0;i<size;i++){
+			hcol = hcol+i;
+			m_shipArray[i]= numToCol(headcol)+= headrow;
+		}
+	}
 }
 
 bool Ship::isDestroyed(){
@@ -40,8 +119,18 @@ bool Ship::isDestroyed(){
 }
 
 bool Ship::isHit(int row, char col){
-
-	return(true);
+	bool Hit=false;
+	std::string hitcoords;
+	std::string hitcol(1,col);
+	hitcoords = hitcol + numToString(row);
+	for(int i=0; i<6; i++){
+		if(m_shipArray[i]=hitcoords){
+			Hit=true;
+			m_hullIntegrity--;
+			isDestroyed();
+		}
+	}
+	return(Hit);
 }
 
 
