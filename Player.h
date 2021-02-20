5f25235fd8882df920299008ce78b0a8fab7a2e5
Chloe Thornton <chloe.kira@gmail.com>
2021-02-20 16:38:05 -0600

    added private array of ships

--- Player.h
+++ Player.h
@@ -21,11 +21,12 @@ class Player
 		bool validatePosition(int row, std::string col, std::string direction, int size);
 		std::string intToString(int num);
 
-		void checkGrid(int shipCoords[]);
+		void checkGrid(std::string shipCoords);
 
 	private:
 		int m_ships;
 		char** playerGrid;
+		Ship shipArray[5];//max number of ships, allows us to have a location for each ship in memory
 
 };
 #endif
