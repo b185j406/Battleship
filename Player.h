9e2c76cdb405d679c6442dc253ad7e895d5ab0a8
Jake Beesley <jakebees@gmail.com>
2021-02-19 22:02:41 -0600

    Added a colToInt function, which takes the corresponding column A-J and translates that letter into a number.
    **Need to implement the validatePosition function**
      -The function needs to make sure the orientation and direction of the ship don't bounce off the array
      -The function needs to know whether a ship is already there
      -If a ship can be placed, update the playerGrid with where the ships are.
    
    Example: Ship starts at A1, its horizontal, its length is 5.
      -A1 can not be taken by another ship.
      -From A1, all elements going 5 to the right aren't taken by another ship either.
      -From A1, going to the right 5 elements doesn't bounce off the array
      -Elements of the ship array would be [A1, B1, C1, D1, E1] as a result.

--- Player.h
+++ Player.h
@@ -13,11 +13,15 @@ class Player
 
 	public:
 		Player(int ships);
+		int colToInt(std::string column);
 		void anchorShips(int length);
 		void showWaters();
 		bool validateCol(std::string column);
 		bool validateRow(int row);
+		bool validatePosition(int row, std::string col, std::string direction, int size);
 		std::string intToString(int num);
+
+		void checkGrid(int shipCoords[]);
 
 	private:
 		int m_ships;
